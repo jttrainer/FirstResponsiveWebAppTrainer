fd794a3c38b7fa18cf64423ab479598d4f722ec7
Jackson Trainer <jttrainer@dmacc.edu>
2021-05-31 15:31:32 -0500

    intital build.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppTrainer.Models;
+
+namespace FirstResponsiveWebAppTrainer.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.year= DateTime.Now.Year;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AgeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Name =model.Name;
+                ViewBag.AgeThisYear = model.AgeThisYear();
+                ViewBag.AgeToday = model.AgeToday();
+            }
+            else
+            {
+                ViewBag.AgeThisYear = 0;
+                ViewBag.AgeToday = 0;
+            }
+            ViewBag.year = DateTime.Now.Year;
+            return View(model);
+        }
+    }
+}
---
+++ Models/AgeModel.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace FirstResponsiveWebAppTrainer.Models
+{
+    public class AgeModel
+    {
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a date of birth.")]
+        [Range(1, 31, ErrorMessage = "Date must be between 1 and 31.")]
+        public int BirthDay { get; set; }
+
+        [Required(ErrorMessage = "Please enter a number for the birth month.")]
+        [Range(1, 12, ErrorMessage = "The number for the month must be between 1 and 12.")]
+        public int BirthMonth { get; set; }
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(0, 9999, ErrorMessage = "The year must be between 0 and 9999.")]
+        public int BirthYear { get; set; }
+
+        //The method will calcualte the age of based on values entered and return the age at the end of this year.
+        public int AgeThisYear()
+        {
+            int currentYear = DateTime.Now.Year;
+            int userAge = currentYear - BirthYear;
+            return userAge;
+        }
+
+        //The method will calcualte the age of based on values entered and return the age as of today.
+        public int AgeToday()
+        {
+            int currentYear = DateTime.Now.Year;
+            int currentMonth = DateTime.Now.Month;
+            int currentDay = DateTime.Now.Day;
+            int userAge;
+            if(BirthMonth <= currentMonth)
+            {
+                if (BirthDay <= currentDay)
+                {
+                    userAge = currentYear - BirthYear;
+                }
+                else
+                {
+                    userAge = currentYear - BirthYear - 1;
+                }
+            }
+            else
+            {
+                userAge = currentYear - BirthYear - 1;
+            }
+            return userAge;
+        }
+    }
+}
